51ebe3bdc81685d5e883e1d26deecd8edca82ba3
mutsuki333 <evanchen333@gmail.com>
2018-05-23 20:00:42 +0800

    server test

--- http/word_rec/word_rec/settings.py
+++ http/word_rec/word_rec/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e8yx2(k4@o2r93^&0!lmt&xrlx&gbr$0_c83f1ropxymqpz4cx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
