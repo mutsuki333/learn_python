b916bf197827e9750c9bf617bac2580f67e3e6eb
Evan <evanchen333@gmail.com>
2018-05-24 00:38:03 +0800

    test google api with django

--- http/word_rec/word_rec/upload/views.py
+++ http/word_rec/word_rec/upload/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
-
 def img_upload(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+        g = G()
+        g.rec(filename)
+        result = g.rec(filename)
         return render(request, 'word_rec/index.html', {
-            'uploaded_file_url': uploaded_file_url
+            'uploaded_file_url': uploaded_file_url,
+            'result': result
         })
     return render(request, 'word_rec/index.html')
-
-# Create your views here.
